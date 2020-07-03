83369045c60b8b0dc76e738e464bdde19def72c7
Daniel Carlozzi <Daniel.Carlozzi@disney.com>
2020-07-03 14:29:20 -0300

    revision

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -35,7 +36,7 @@ namespace ExpensasAbbinatura
                     .AddRazorRuntimeCompilation();
 
             services.AddDbContext<ExpensasContext>(
-                options => options.UseMySql(Configuration.GetConnectionString("ExpensasContext"), 
+                options => options.UseMySql(Configuration.GetConnectionString("ExpensasContext"),
                     mysqlOptions => mysqlOptions.ServerVersion(new Version(8, 0, 18), ServerType.MySql))
             );
 
@@ -69,7 +70,14 @@ namespace ExpensasAbbinatura
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseStaticFiles();
+            //github
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".revision"] = "application/x-msdownload";
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ServeUnknownFileTypes = true,
+                ContentTypeProvider = provider
+            });
 
             app.UseRouting();
 
@@ -81,10 +89,10 @@ namespace ExpensasAbbinatura
             app.UseAuthentication();
 
             app.UseAuthorization();
-            
+
             var cookiePolicyOptions = new CookiePolicyOptions
             {
-                MinimumSameSitePolicy = SameSiteMode.Strict,                
+                MinimumSameSitePolicy = SameSiteMode.Strict,
             };
             app.UseCookiePolicy(cookiePolicyOptions);
 
@@ -92,6 +100,10 @@ namespace ExpensasAbbinatura
             {
                 endpoints.MapRazorPages();
             });
+
+
+
+
         }
     }
 }
